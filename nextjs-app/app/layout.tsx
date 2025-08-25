ee0504dcdc05ffff22ab2f4ae0ee9936ad7f6116
thanhhoan99 <thanhhoan7878647@gmail.com>
2025-08-25 21:40:29 +0700

    update apply

--- nextjs-app/app/layout.tsx
+++ nextjs-app/app/layout.tsx
@@ -3,6 +3,8 @@ import "@/styles/globals.css";
 import { NextAuthProvider } from "@/components/providers/authProviders";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import type { Metadata } from "next";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const plusJakartaSans = Plus_Jakarta_Sans({
   subsets: ["latin"],
@@ -25,6 +27,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${plusJakartaSans.className}`}>
         <NextAuthProvider>{children}</NextAuthProvider>
+         <ToastContainer position="top-right" autoClose={4000} />
       </body>
     </html>
   );
