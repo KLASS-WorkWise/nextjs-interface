199de235171ee6b7c2e9080b1c56bf0f5ca115f1
nvpho0710 <phonguyenvan71023@gmail.com>
2025-09-19 13:29:08 +0700

    fix conflict update

--- nextjs-app/app/job-details-2/[id]/page.tsx
+++ nextjs-app/app/job-details-2/[id]/page.tsx
@@ -262,6 +262,7 @@ export default function JobDetails2() {
                               onSuccess={() => toast.success("Applied successfully!")}
                             />
                           )}
+                          
                 <div className="col-lg-4 col-md-12 col-sm-12 col-12 pl-40 pl-lg-15 mt-lg-30">
                   {/* <div className="sidebar-border">
                     <div className="sidebar-heading">
